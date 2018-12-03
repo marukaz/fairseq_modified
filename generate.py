4f3504ee57009d9878367271a30dbf6c1a2e9474
Kazuki <marukaz.jh@gmail.com>
2018-12-03 23:44:20 +0900

    fix json style

--- generate.py
+++ generate.py
@@ -110,6 +110,7 @@ def main(args):
         wps_meter = TimeMeter()
         for (sample_id, src_tokens, target_tokens, hypos), (_, _, _, ans_hypo) in zip(translations, answers):
             d = {}
+            d['id'] = int(sample_id)
             # Process input and ground truth
             has_target = target_tokens is not None
             target_tokens = target_tokens.int().cpu() if has_target else None
@@ -174,7 +175,7 @@ def main(args):
             wps_meter.update(src_tokens.size(0))
             t.log({'wps': round(wps_meter.avg)})
             num_sentences += 1
-            print(f'{{{sample_id}: {json.dumps(d, ensure_ascii=False)}}}')
+            print(json.dumps(d, ensure_ascii=False))
 
     # print('| Translated {} sentences ({} tokens) in {:.1f}s ({:.2f} sentences/s, {:.2f} tokens/s)'.format(
     #     num_sentences, gen_timer.n, gen_timer.sum, num_sentences / gen_timer.sum, 1. / gen_timer.avg))
