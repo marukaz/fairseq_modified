699905d1fa2cb323ce6117983e7e860aae18888d
Kazuki <marukaz.jh@gmail.com>
2018-12-05 15:50:37 +0900

    gitignore

--- build/lib.linux-x86_64-3.6/fairseq/test.py
+++
@@ -1,2 +0,0 @@
-def test_print(s):
-  print(s)
--- job_sh/gen_beam_transformer.sh
+++ job_sh/gen_beam_transformer.sh
@@ -2,7 +2,7 @@
 ## current working directory
 #$ -cwd
 #$ -l q_node=1
-#$ -l h_rt=01:00:00
+#$ -l h_rt=24:00:00
 #$ -N beam
 #$ -m abe
 #$ -M kopamaru@gmail.com
@@ -15,11 +15,12 @@
 module load cuda/9.0.176
 module load cudnn/7.3
 
-source ~/fairseq/venv/bin/activate
+source ~/fairseq_modified/venv/bin/activate
 
-beam=30; \
-python ~/fairseq/generate.py /gs/hs0/tga-nlp-titech/takase/headline/data/jnc_preprocessed4fairseq_bin/jnc_1snt_spm_headline/ \
+beam=31; subset="train"; \
+python ~/fairseq_modified/generate.py /gs/hs0/tga-nlp-titech/takase/headline/data/jnc_preprocessed4fairseq_bin/jnc_1snt_spm_headline/ \
 --path /gs/hs0/tga-nlp-titech/takase/headline/exp/jnc_spm_fairseq_transformer_wmtendesetting/jnc_spm_1snt_dropout0.1_gpu4_updatefreq2/checkpoint_best.pt \
+--gen-subset train \
 --batch-size 64 \
 --beam ${beam} \
---nbest ${beam} | tee ~/fairseq/gen/beam${beam}_wmt_d01_gpu4_updatefreq2.out
+--nbest ${beam} | tee ~/fairseq_modified/gen/beam${beam}_${subset}_wmt_d01_gpu4_updatefreq2.json
