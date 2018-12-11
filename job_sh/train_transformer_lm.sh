394a324d108b3d3db97afc6124ebb55fe10a212f
Kazuki Matsumaru <marukaz.jh@gmail.com>
2018-12-12 02:22:49 +0900

    new job

---
+++ job_sh/train_transformer_lm.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+## current working directory
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=24:00:00
+#$ -N train_transformer_lm
+#$ -m abe
+#$ -M kopamaru@gmail.com
+#$ -o o.train_transformer_lm
+#$ -e e.train_transformer_lm
+
+## Initialize module command (don't remove)
+. /etc/profile.d/modules.sh
+
+module load cuda/9.0.176
+module load cudnn/7.3
+
+source ~/fairseq_modified/venv/bin/activate
+
+python ~/fairseq_modified/train.py /gs/hs0/tga-nlp-titech/matsumaru/data/jnc_fairseq_only_headline_200k_test_bin \
+--arch transformer_lm \
+--max-epoch 15 \
+--save-dir /gs/hs0/tga-nlp-titech/matsumaru/entasum/fairseq_model/jnc_tgt_transformer_lm_200k_test
