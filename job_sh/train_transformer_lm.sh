4c4b9aa556cec8a585f315e6c08fd8efeb71dd7b
Kazuki Matsumaru <marukaz.jh@gmail.com>
2018-12-12 23:22:40 +0900

    lm

--- job_sh/train_transformer_lm.sh
+++ job_sh/train_transformer_lm.sh
@@ -19,5 +19,5 @@ source ~/fairseq_modified/venv/bin/activate
 
 python ~/fairseq_modified/train.py --task language_modeling /gs/hs0/tga-nlp-titech/matsumaru/data/jnc_fairseq_only_headline_200k_test_bin \
 --arch transformer_lm \
---max-epoch 20 \
+--max-epoch 15 \
 --save-dir /gs/hs0/tga-nlp-titech/matsumaru/entasum/fairseq_model/jnc_tgt_transformer_lm_200k_test
