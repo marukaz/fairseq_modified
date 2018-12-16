6549dc1c66e6e2d6eb697e343093ee7654cb3a6d
Kazuki Matsumaru <marukaz.jh@gmail.com>
2018-12-16 17:25:43 +0900

    make eval_lm_scores

---
+++ eval_lm_scores.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3 -u
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+
+"""
+Evaluate the perplexity of a trained language model.
+"""
+
+import numpy as np
+import torch
+
+from fairseq import options, progress_bar, tasks, utils
+from fairseq.meters import StopwatchMeter, TimeMeter
+from fairseq.sequence_scorer import SequenceScorer
+
+
+class WordStat(object):
+    def __init__(self, word, is_bpe):
+        self.word = word
+        self.is_bpe = is_bpe
+        self.log_prob = 0
+        self.next_word_prob = 0
+        self.count = 0
+        self.missing_next_words = 0
+
+    def add(self, log_prob, next_word_prob):
+        """ increments counters for the sum of log probs of current word and next
+            word (given context ending at current word). Since the next word might be at the end of the example,
+            or it might be not counted because it is not an ending subword unit,
+            also keeps track of how many of those we have seen """
+        if next_word_prob is not None:
+            self.next_word_prob += next_word_prob
+        else:
+            self.missing_next_words += 1
+        self.log_prob += log_prob
+        self.count += 1
+
+    def __str__(self):
+        return '{}\t{}\t{}\t{}\t{}\t{}'.format(self.word, self.count, self.log_prob, self.is_bpe,
+                                               self.next_word_prob, self.count - self.missing_next_words)
+
+
+def main(parsed_args):
+    assert parsed_args.path is not None, '--path required for evaluation!'
+
+    use_cuda = torch.cuda.is_available() and not parsed_args.cpu
+
+    task = tasks.setup_task(parsed_args)
+
+    # Load ensemble
+    models, args = utils.load_ensemble_for_inference(parsed_args.path.split(':'), task,
+                                                     model_arg_overrides=eval(parsed_args.model_overrides))
+
+    for arg in vars(parsed_args).keys():
+        if arg not in {'self_target', 'future_target', 'past_target', 'tokens_per_sample', 'output_size_dictionary'}:
+            setattr(args, arg, getattr(parsed_args, arg))
+    task = tasks.setup_task(args)
+
+    # Load dataset splits
+    task.load_dataset(args.gen_subset)
+
+    # Optimize ensemble for generation and set the source and dest dicts on the model (required by scorer)
+    for model in models:
+        model.make_generation_fast_()
+        if args.fp16:
+            model.half()
+
+    assert len(models) > 0
+
+    itr = task.get_batch_iterator(
+        dataset=task.dataset(args.gen_subset),
+        max_tokens=args.max_tokens or 36000,
+        max_sentences=args.max_sentences,
+        max_positions=utils.resolve_max_positions(*[
+            model.max_positions() for model in models
+        ]),
+        num_shards=args.num_shards,
+        shard_id=args.shard_id,
+        ignore_invalid_inputs=True,
+    ).next_epoch_itr(shuffle=False)
+
+    scorer = SequenceScorer(models, task.target_dictionary)
+    if use_cuda:
+        scorer.cuda()
+
+    with progress_bar.build_progress_bar(args, itr) as t:
+        results = scorer.score_batched_itr(t, cuda=use_cuda)
+        for sample_id, src_tokens, __, hypos in results:
+            for hypo in hypos:
+                score = hypo['score']
+                inf_scores = score.eq(float('inf')) | score.eq(float('-inf'))
+                if inf_scores.any():
+                    print('| Skipping tokens with inf scores:',
+                          task.target_dictionary.string(hypo['tokens'][inf_scores.nonzero()]))
+                    score = score[(~inf_scores).nonzero()]
+            print(sample_id, score)
+
+
+if __name__ == '__main__':
+    parser = options.get_eval_lm_parser()
+    args = options.parse_args_and_arch(parser)
+    main(args)
