39e033cc46e94f6d339956834414f3f65c5dc75d
Kazuki <marukaz.jh@gmail.com>
2019-01-02 19:56:11 +0900

    update job.sh

--- job_sh/gen_lm_scores.sh
+++ job_sh/gen_lm_scores.sh
@@ -2,7 +2,7 @@
 ## current working directory
 #$ -cwd
 #$ -l f_node=1
-#$ -l h_rt=01:00:00
+#$ -l h_rt=24:00:00
 #$ -N lm_scores
 #$ -m abe
 #$ -M kopamaru@gmail.com
@@ -18,7 +18,7 @@ module load cudnn/7.3
 source ~/fairseq_modified/venv/bin/activate
 
 path="/gs/hs0/tga-nlp-titech/matsumaru/entasum/fairseq_model/jnc_tgt_transformer_lm_200k_test"; \
-python ~/fairseq_modified/eval_lm_scores.py /gs/hs0/tga-nlp-titech/matsumaru/data/jnc_fairseq_dbs63_3ktest_from_valid_tf_200ktest_bin \
+python ~/fairseq_modified/eval_lm_scores.py /gs/hs0/tga-nlp-titech/matsumaru/data/jnc_fairseq_dbs63_tf_200ktest_bin \
 --path ${path}/checkpoint_best.pt \
 --sample-break-mode eos \
-> ${path}_gen/jnc_fairseq_dbs63_3ktest_from_valid_tflm_200ktest.out
+> ${path}_gen/jnc_fairseq_dbs63_200ktest_tflm_200ktest.out
